bd0b7bec60e919f1a2291fde06dfe88bd6363a6f
actyvystom <thomas.goerldt@actyvyst.com>
2023-06-14 22:44:48 +0200

    try another deployment

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <Heading>🐬 Capstone Template 🐬</Heading>
-      <SubHeading>We dolphinitely love testing!</SubHeading>
+      <SubHeading>We dolphinitely love coding!</SubHeading>
     </main>
   );
 }
